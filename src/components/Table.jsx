dc8c1b2f76329d2de6642f9f189749cf4e9704ab
Savano Miatama <vano_202228@yahoo.com>
2024-03-11 02:28:39 +0700

    feat: create registry page - add table component

---
+++ src/components/Table.jsx
@@ -0,0 +1,131 @@
+const Table = ({
+  headers,
+  data: tableData,
+  children
+}) => {
+  let tableHeaders
+  if (headers) {
+    tableHeaders = headers.map(
+      ({ key, label }, index) => {
+        if (key !== 'actions') {
+          return (
+            <th
+              key={`header-${key}-${index}`}
+              scope="col"
+              className="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase"
+            >
+              {label}
+            </th>
+          )
+        } else {
+          return (
+            <th
+              key={`header-${key}-${index}`}
+              scope="col"
+              className="p-4"
+            >
+              <span className="sr-only">
+                Actions
+              </span>
+            </th>
+          )
+        }
+      }
+    )
+  }
+
+  let tableBody
+  if (tableData) {
+    tableBody = tableData.map(
+      (item, indexItem) => {
+        let tds
+        if (headers) {
+          tds = headers.map(({ key }) => {
+            if (key !== 'actions') {
+              return (
+                <td
+                  key={`data-${key}-${indexItem}`}
+                  className="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap"
+                >
+                  {item[key]}
+                </td>
+              )
+            } else {
+              return (
+                <td
+                  key={`data-${key}-${indexItem}`}
+                  className="py-4 px-6 text-sm font-medium text-right whitespace-nowrap"
+                >
+                  {children.actions}
+                </td>
+              )
+            }
+          })
+        }
+        return (
+          <tr
+            key={`data-row-${indexItem}`}
+            className="hover:bg-gray-100"
+          >
+            <td
+              key={`data-checkbox-${indexItem}`}
+              className="p-4 w-4"
+            >
+              <div className="flex items-center">
+                <input
+                  id="checkbox-table-1"
+                  type="checkbox"
+                  className="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 focus:ring-2"
+                />
+                <label
+                  htmlFor="checkbox-table-1"
+                  className="sr-only"
+                >
+                  checkbox
+                </label>
+              </div>
+            </td>
+            {tds}
+          </tr>
+        )
+      }
+    )
+  }
+
+  return (
+    <>
+      <div className="overflow-x-auto shadow-md sm:rounded-lg">
+        <div className="inline-block min-w-full align-middle">
+          <div className="overflow-hidden ">
+            <table className="min-w-full divide-y divide-gray-200 table-fixed">
+              <thead className="bg-gray-100">
+                <tr>
+                  <th scope="col" className="p-4">
+                    <div className="flex items-center">
+                      <input
+                        id="checkbox-all"
+                        type="checkbox"
+                        className="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 focus:ring-2"
+                      />
+                      <label
+                        htmlFor="checkbox-all"
+                        className="sr-only"
+                      >
+                        checkbox
+                      </label>
+                    </div>
+                  </th>
+                  {tableHeaders}
+                </tr>
+              </thead>
+              <tbody className="bg-white divide-y divide-gray-200">
+                {tableBody}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+export default Table
--- src/pages/RegistryPage.jsx
+++ src/pages/RegistryPage.jsx
@@ -1,11 +1,62 @@
+import Table from '../components/Table'
+
 export default function RegistryPage() {
+  const headers = [
+    {
+      key: 'company_name',
+      label: 'Nama'
+    },
+    {
+      key: 'npwp',
+      label: 'NPWP'
+    },
+    {
+      key: 'contact_person',
+      label: 'Kontak'
+    },
+    {
+      key: 'status',
+      label: 'Status'
+    },
+    {
+      key: 'actions',
+      label: ''
+    }
+  ]
+  const tableData = [
+    {
+      company_name: 'Asdf',
+      npwp: '123456789012345',
+      contact_person: 'John Doe',
+      status: 'John Doe'
+    }
+  ]
+
   return (
     <div className="w-screen min-h-screen overflow-auto w-full bg-gradient-to-r from-teal-400 to-cyan-500">
-      <div className="h-screen w-1/2 mx-auto bg-neutral-200 p-8">
-        <div className="text-center">
-          <h2 className="text-lg">
-            Registrasi page
-          </h2>
+      <div className="h-screen w-1/2 mx-auto bg-neutral-200 p-8 rounded-md border-slate-500 shadow-md shadow-cyan-500">
+        <h2 className="text-2xl font-semibold capitalize mb-4 text-gray-700">
+          Cek proses pendaftaran
+        </h2>
+        <div className="mb-6">
+          <input
+            className="focus:outline-none focus:ring-0 focus:border-blue-200/75 focus:shadow-md focus:shadow-blue-500/30 border border-slate-100 rounded-md py-1 px-2 w-full h-12"
+            placeholder="Masukkan NPWP"
+          />
+        </div>
+        <div>
+          <Table
+            headers={headers}
+            data={tableData}
+          >
+            {{
+              actions: (
+                <>
+                  <div>Edit</div>
+                </>
+              )
+            }}
+          </Table>
         </div>
       </div>
     </div>

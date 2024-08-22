c26b33d971bc7fc157dfb8e684be6f7909d9dc12
savano maitama <savano.miatama@gmail.com>
2024-08-22 22:16:08 +0700

    feat: add back link on edit form admin

--- src/pages/AdminEditPage.jsx
+++ src/pages/AdminEditPage.jsx
@@ -1,22 +1,7 @@
+import { Link } from 'react-router-dom'
 import RegistrationsForm from '/src/components/RegistrationsForm'
 
 export default function AdminEditPage() {
-
-  const footer = (
-    <div className="flex gap-4">
-      <button
-        className="py-1 px-3 text-slate rounded-lg active:bg-slate-500/50 shadow-md shadow-slate-500/30 focus:outline-none focus:ring-0 focus:border-blue-500 focus:shadow-lg focus:shadow-slate-500/30 hover:bg-slate-500/10 hover:text-slate-500 disabled:bg-blue-500/20 disabled:text-slate-400"
-      >
-        Cancel
-      </button>
-      <button
-        className="py-1 px-3 text-white rounded-lg active:bg-blue-500/50 bg-blue-500 shadow-md shadow-slate-500/30 focus:outline-none focus:ring-0 focus:border-blue-500 focus:shadow-lg focus:shadow-slate-500/30 hover:bg-blue-500/20 hover:text-blue-500 disabled:bg-blue-500/20 disabled:text-slate-400"
-      >
-        Save
-      </button>
-    </div>
-  )
-
   return (
     <>
       <div className="flex w-screen min-h-screen overflow-auto justify-center items-center bg-white">
@@ -25,6 +10,14 @@ export default function AdminEditPage() {
             <h2 className="text-2xl text-slate-700 font-semibold hidden md:block">
               Edit registrasi
             </h2>
+
+            <Link
+              as="a"
+              to="/admin/"
+              className="ml-1 text-blue-600 hover:text-blue-500/30"
+            >
+              Back
+            </Link>
           </div>
 
           <RegistrationsForm />

055a86068549bda282760cf37b9331abd4592038
savano maitama <savano.miatama@gmail.com>
2024-09-02 00:19:34 +0700

    chore: add sentry for visualizing production error

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,22 @@ import App from './App.jsx'
 import { Provider } from 'react-redux'
 import { store } from './store/index.js'
 import './index.css'
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://016caf06390afe4f270654070e13b37d@o4507876648943616.ingest.us.sentry.io/4507876651565056",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  // Tracing
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", /^https:\/\/member.gakindo\.or.id/],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,21 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), sentryVitePlugin({
+    org: "savano-miatama",
+    project: "javascript-react"
+  })],
+
   resolve: {
     alias: {
       src: '/src'
     }
+  },
+
+  build: {
+    sourcemap: true
   }
 })
